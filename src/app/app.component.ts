024bbe20b134d68c0fd3b13773cab4fd9fc69bc5
gpdoud <gpdoud@gmail.com>
2018-01-23 18:21:39 -0500

    Very little change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-<p>Hello, world!</p>
+    <p>Hello, world!</p>
     
-  `,
-  styles: []
+  `
 })
 export class AppComponent {
   title = 'app';
