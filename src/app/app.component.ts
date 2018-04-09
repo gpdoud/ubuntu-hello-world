d85258ef5bc92d6f42ea16060e13093b75d27690
gpdoud <gpdoud@gmail.com>
2018-04-09 07:48:59 -0400

    Updated to bind name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <p>Hello, world!</p>
+    <p>Hello, {{name}}!</p>
     
   `
 })
 export class AppComponent {
-  title = 'app';
+  name = 'Greg';
 }
